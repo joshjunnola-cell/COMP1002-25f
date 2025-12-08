69956793d6f21a4e2edcb2eff32d21131257b1aa
Josh Junnola <joshjunnola@gmail.com>
2025-12-08 00:30:00 -0500

    trying to add java function

--- Project-1/js/nav.js
+++ Project-1/js/nav.js
@@ -1,4 +1,4 @@
-const pageURL = window.location.href;
+const pageURL = window.location.href; //from class
 
 const navLinks = document.querySelectorAll("header nav > ul > li > a");
 
@@ -12,4 +12,31 @@ navLinks.forEach((element) => {
 
         element.classList.add("current");
     };
+});
+
+const topHover = document.getElementById("topHov");
+const bottomHover = document.getElementById("bottomHov");
+
+topHover.addEventListener("mouseenter", () => {
+    topHover.textContent = "ORDER PAGE!";
+});
+
+topHover.addEventListener("mouseleave", () => {
+    topHover.textContent = "SALE!";
+});
+
+topHover.addEventListener("click", () => {
+    window.location.href = "order.html";
+});
+
+bottomHover.addEventListener("mouseenter", () => {
+    bottomHover.textContent = "INFO PAGE!";
+});
+
+bottomHover.addEventListener("mouseleave", () => {
+    bottomHover.textContent = "LEARN MORE!";
+});
+
+bottomHover.addEventListener("click", () => {
+    window.location.href = "info.html";
 });
