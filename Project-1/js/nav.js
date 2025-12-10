43129ab2e9694479be8e14f22afff7f3585d27f7
Josh Junnola <joshjunnola@gmail.com>
2025-12-09 20:38:22 -0500

    got links working

--- Project-1/js/nav.js
+++ Project-1/js/nav.js
@@ -18,19 +18,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const topHalf = document.getElementById("topHalf");
     const bottomHalf = document.getElementById("bottomHalf");
 
-    topHalf.ad("mouseover", () => {
-        topHalf.style.background = "linear-gradient(to bottom, rgba(255, 255, 0, 0.5), rgba(255, 255, 0, 0))";
+    topHalf.addEventListener("mouseover", () => {
         topHalf.textContent = "On Sale Now!";
     });
     topHalf.addEventListener("click", () => {
         window.location.href = "order.html";
     });
+    topHalf.addEventListener("mouseout", () => {
+        topHalf.textContent = "";
+    });
 
     bottomHalf.addEventListener("mouseover", () => {
-        bottomHalf.style.background = "linear-gradient(to top, rgba(255, 0, 0, 0.5), rgba(255, 0, 0, 0))";
         bottomHalf.textContent = "Learn More!";
     });
     bottomHalf.addEventListener("click", () => {
         window.location.href = "info.html";
+    });
+    bottomHalf.addEventListener("mouseout", () => {
+        bottomHalf.textContent = "";
     });
 });
